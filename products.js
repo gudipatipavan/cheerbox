521b4f55276f78fcb38fa8280a38b7eeb5986a53
gudipatipavan <pavangudipatis@gmail.com>
2025-06-21 15:47:30 +0530

    products.js

---
+++ products.js
@@ -0,0 +1,41 @@
+Product ID	Product Name	url	Selling Price
+CB001	Lock Dairy	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Lock%20Dairy.jpeg	199
+CB002	Fancy Dairy	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Fancy%20Dairy.jpeg	250
+CB003	Box Lock Dairy	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Box%20Lock%20Dairy.jpeg	199
+CB007	Mosquito Repellent Spinner Wrist Band	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Mosquito%20Repellent%20Spinner%20Wrist%20Band.jpeg	120
+CB008	Mosquito Repellent Round Wrist Band	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Mosquito%20Repellent%20Round%20Wrist%20Band.jpeg	99
+CB009	DIY Diamond Painting	https://raw.github.com/gudipatipavan/cheerbox/blob/main/DIY%20Diamond%20Painting.jpeg	49
+CB010	Macarom Photoframe Diamond Painting	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Macarom%20Photoframe%20Diamond%20Painting.jpeg	70
+CB012	candy flower	https://raw.github.com/gudipatipavan/cheerbox/blob/main/candy%20flower.jpeg	100
+CB013	sling bags	https://raw.github.com/gudipatipavan/cheerbox/blob/main/sling%20bag%20Frozen.jpeg	300
+CB013	sling bags	https://raw.github.com/gudipatipavan/cheerbox/blob/main/sling%20bag%20Ironman.jpeg	300
+CB013	sling bags	https://raw.github.com/gudipatipavan/cheerbox/blob/main/sling%20bag%20Kitty.jpeg	300
+CB013	sling bags	https://raw.github.com/gudipatipavan/cheerbox/blob/main/sling%20bag%20Space.jpeg	300
+CB014	3D cartoon Hard Pencil Case - Big	https://raw.github.com/gudipatipavan/cheerbox/blob/main/3D%20cartoon%20Hard%20Pencil%20Case%20-%20Big%20Ironman.jpeg	200
+CB015	3D cartoon Hard Pencil Case - Classic	https://raw.github.com/gudipatipavan/cheerbox/blob/main/3D%20cartoon%20Hard%20Pencil%20Case%20-%20Classic%20unicorn.jpeg	250
+CB015	3D cartoon Hard Pencil Case - Classic	https://raw.github.com/gudipatipavan/cheerbox/blob/main/3D%20cartoon%20Hard%20Pencil%20Case%20-%20Classic%20Captain%20america.jpeg	250
+CB016	3D cartoon Hard Pencil Case - Small	https://raw.github.com/gudipatipavan/cheerbox/blob/main/3D%20cartoon%20Hard%20Pencil%20Case%20-Small.jpeg	149
+CB017	Nail Art	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Nail%20Art.jpeg	69
+CB018	School Bags	https://raw.github.com/gudipatipavan/cheerbox/blob/main/School%20Bag%20My%20Melody.jpeg	350
+CB018	School Bags	https://raw.github.com/gudipatipavan/cheerbox/blob/main/School%20Bag%20Sofia.jpeg	350
+CB018	School Bags	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Spiderman%20bag%20sold%20out.png	350
+CB018	School Bags	https://raw.github.com/gudipatipavan/cheerbox/blob/main/School%20Bag%20Spiderman.jpeg	350
+CB019	Happy Learning Stationery Set	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Happy%20Learning%20Stationery%20Set-%20Blue.jpeg	99
+CB019	Happy Learning Stationery Set	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Happy%20Learning%20Stationery%20Set-Pink.jpeg	99
+CB021	DIY Drawing Set	https://raw.github.com/gudipatipavan/cheerbox/blob/main/DIY%20Drawing%20Set.jpeg	99
+CB024	Peggy Bank	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Peggy%20Bank.jpeg	199
+CB025	Nauty Bear Water bottle	https://raw.github.com/gudipatipavan/cheerbox/blob/main/bear%20waterbottle.jpeg	249
+CB026	Fresh Kitty Water bottle	https://raw.github.com/gudipatipavan/cheerbox/blob/main/fresh%20waterbottle.jpeg	149
+CB027	Rainbow Water bottle	https://raw.github.com/gudipatipavan/cheerbox/blob/main/colourwaterbottle.jpeg	169
+CB029	Synthetic Book Cover	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Synthetic%20Book%20Cover.jpeg	99
+CB033	Book Stickers/labels	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Book%20Stickerslabels.jpeg	15
+CB034	2 in 1 use Double Step Lunch Box	https://raw.github.com/gudipatipavan/cheerbox/blob/main/2%20in%201%20luch%20box%20sold%20out.png	120
+CB035	Thermal Lunch Bags	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Thermal%20Lunch%20Bag.jpeg	145
+CB035	Thermal Lunch Bags	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Thermal%20Lunch%20Bags.jpeg	145
+CB036	Apsara Exam Pads	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Apsara%20Exam%20Pads.jpeg	70
+CB037	Disposable Tube Paper Soap in Flavour Design	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Disposable%20Tube%20Paper%20Soap%20in%20Flavour%20Design.jpg	50
+CB038	Kids Avenger Watch	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Kids%20Avenger%20Watch.jpeg	149
+CB039	Kids Batman Watch	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Kids%20Batman%20Watch.jpeg	149
+CB040	Kids Digital Smart Army Watch	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Kids%20Digital%20Smart%20Army%20watch.jpeg	199
+CB041	Soft Toys	https://raw.github.com/gudipatipavan/cheerbox/blob/main/Soft%20Toys.jpeg	99
+CB042	6-in-1 Pattern Roller Linear Color Pen	https://raw.github.com/gudipatipavan/cheerbox/blob/main/pattern.png	129
